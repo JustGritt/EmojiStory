8541f89703167ef73031c6ca2469be9ac4926180
Alexis TAN <alexistan9712@gmail.com>
2023-11-07 16:24:09 +0100

    Add monorepo

---
+++ front/app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+import { io, Socket } from "socket.io-client";
+import { ClientToServerEvent, ServerToClientEvent } from 'interface/event';
+
+const socket: Socket<ServerToClientEvent, ClientToServerEvent> = io(
+  process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000"
+);
+export default function Home() {
+  useEffect(() => {
+    socket.on("story-update", (data) => {
+      console.log({ data });
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24">
+      <h1 className="text-6xl font-bold">Emoji Story</h1>
+    </main>
+  );
+}
---
+++ server/src/emoji/emoji.gateway.ts
@@ -0,0 +1,34 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { ClientToServerEvent, ServerToClientEvent } from 'interface/event';
+
+type _Socket = Socket<ServerToClientEvent, ClientToServerEvent>;
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+    credentials: true,
+  },
+})
+export class EmojiGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: _Socket;
+
+  clients: Map<Socket, { [key: string]: any }> = new Map();
+
+  handleConnection(client: Socket) {
+    this.clients.set(client, {});
+
+    client.emit('story-update', {});
+  }
+
+  handleDisconnect(client: Socket) {
+    this.clients.delete(client);
+  }
+}
